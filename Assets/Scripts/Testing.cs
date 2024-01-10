5b32a0ced28cfe9be7745250127df5c4a1ac0407
Awildtse <atse9288@gmail.com>
2024-01-09 16:59:47 -0800

    Created GridDebugObject so we can visualize the gridlayout.

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -10,7 +10,6 @@ public class Testing : MonoBehaviour
     {
         gridSystem = new GridSystem(10, 10, 2f);
         gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
-        Debug.Log(new GridPosition(5, 7));
     }
 
     private void Update()
